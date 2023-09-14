31cbb26049386b186db45369d186f70c9b1c2def
peihsuan <june625678@gmail.com>
2023-09-15 01:28:09 +0800

    完成手風琴

---
+++ yongxsin.test/frontEndPackage/js/carousel.js
@@ -0,0 +1,25 @@
+// 首頁大圖輪播
+
+$(".banner-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    dots: true,
+    items: 1,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+});
+
+
+//成功案例輪播
+
+
+// $(".case-carousel").owlCarousel({
+//     loop: true,
+//     autoplay: true,
+//     dots: false,
+//     items: 1,
+//     margin:0,
+//     autoplayTimeout: 2000,
+
+// });
+
