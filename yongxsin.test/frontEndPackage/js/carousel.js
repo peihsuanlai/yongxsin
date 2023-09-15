2c84c0cbb63d510230a4bccf40f3867ec44874d0
peihsuan <131450861+peihsuanlai@users.noreply.github.com>
2023-09-15 18:35:41 +0800

    9/15

--- yongxsin.test/frontEndPackage/js/carousel.js
+++ yongxsin.test/frontEndPackage/js/carousel.js
@@ -17,9 +17,33 @@ $(".banner-carousel").owlCarousel({
 //     loop: true,
 //     autoplay: true,
 //     dots: false,
-//     items: 1,
+//     items: 5,
 //     margin:0,
 //     autoplayTimeout: 2000,
 
 // });
 
+
+
+
+$(".faqtab-carousel").owlCarousel({
+    loop: false,
+    autoplay: false,
+    dots: false,
+    margin: 0,
+    autoWidth:true,
+    responsive:{
+        0:{
+            items:3,
+            nav:true
+        },
+        768:{
+            items:4,
+            nav:true
+        },
+        992:{
+            items:5,
+            nav:true,
+        }
+    }
+});
