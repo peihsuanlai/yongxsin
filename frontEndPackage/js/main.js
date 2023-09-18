fe980665e01a3b6ec78904b9fc2ea3870f1606d6
peihsuan <june625678@gmail.com>
2023-09-18 23:57:34 +0800

    9/18回家調整浮動按鈕

--- frontEndPackage/js/main.js
+++ frontEndPackage/js/main.js
@@ -1,10 +1,3 @@
-//floating buttons control
-$(".expand-circle").click(function () {
-    $(this).toggleClass("icon-rotate");
-    $(".line-icon").toggleClass("d-none");
-    $(".call-icon").toggleClass("d-none");
-});
-
 //page top
 
 $(".desktop-page-top-btn, .mobile-page-top-btn").click(function (e) {
@@ -16,6 +9,7 @@ $(".desktop-page-top-btn, .mobile-page-top-btn").click(function (e) {
         500
     ); //500為0.5秒，為animate裡用來設定滑動到最上方(scrollTop:0)時的秒數
 });
+
 
 //url control
 $(function () {
